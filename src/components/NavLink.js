e821c2c3db9d7a2f24bcacf416b301688ee66bb8
sarazaToussaint <tmsaraza@gmail.com>
2022-06-21 15:37:08 +0200

    Fix style active link error

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 
-const NavLink = () => {
-  const navLinkStyle = ({ isActive }) => ({
-    return: {
-      textDecoration: isActive ? 'bold' : 'normal',
-    },
-  });
-
-  return (
-    <nav>
-      <Link style={navLinkStyle} to="/">MyProfile</Link>
-      <Link style={navLinkStyle} to="/Rockets">Rockets</Link>
-      <Link style={navLinkStyle} to="/Missions">Missions</Link>
-    </nav>
-  );
-};
+const NavLink = () => (
+  <nav>
+    <Link to="/">MyProfile</Link>
+    <Link to="/Rockets">Rockets</Link>
+    <Link to="/Missions">Missions</Link>
+  </nav>
+);
 
 export default NavLink;
