cff69f74047d28a7b1455ba9d8c935d53eebfa3e
sarazaToussaint <tmsaraza@gmail.com>
2022-06-21 22:12:46 +0200

    style navbar

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
+import planet from '../images/planet.png';
 
 const NavLink = () => (
-  <nav>
-    <Link to="/">MyProfile</Link>
-    <Link to="/Rockets">Rockets</Link>
-    <Link to="/Missions">Missions</Link>
-  </nav>
+  <div className="navContainer">
+    <di className="logoContainer">
+      <img src={planet} alt="Planet" className="logo" />
+      <p className="logoTitle">Space Travelers Hub</p>
+    </di>
+    <nav>
+      <Link to="/">MyProfile</Link>
+      <Link to="/Rockets">Rockets</Link>
+      <Link to="/Missions">Missions</Link>
+    </nav>
+  </div>
 );
 
 export default NavLink;
