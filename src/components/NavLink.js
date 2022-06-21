883ca0a0b2686349d7e64bd6a8cf1bef370779e3
sarazaToussaint <tmsaraza@gmail.com>
2022-06-21 14:57:40 +0200

    Style active link

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavLink = () => (
-  <nav>
-    <Link to="/">MyProfile</Link>
-    <Link to="/Rockets">Rockets</Link>
-    <Link to="/Missions">Missions</Link>
-  </nav>
-);
+const NavLink = () => {
+  const navLinkStyle = ({ isActive }) => ({
+    return: {
+      textDecoration: isActive ? 'bold' : 'normal',
+    },
+  });
+
+  return (
+    <nav>
+      <Link style={navLinkStyle} to="/">MyProfile</Link>
+      <Link style={navLinkStyle} to="/Rockets">Rockets</Link>
+      <Link style={navLinkStyle} to="/Missions">Missions</Link>
+    </nav>
+  );
+};
 
 export default NavLink;
